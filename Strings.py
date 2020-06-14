bd89664757a2339088bbab1a6041349a160393be
raghu <raghumina11@gmail.com>
2020-06-14 18:30:35 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,15 @@
+# In this module i am working on some basic bioinformatics concepts such as
+# working on bio sequences
+dna = "atgcccgcgcgatagagatatagcgacacgacgatcatc"
+print(dna.count("c"))  # to find the number of specific strands in DNA
+
+# and for the position or location we use
+print(dna[-1])  # for the last position
+print(dna[0])   # for the 1st position
+print(dna[:3])  # upto 3 sequeces start from 0
+print(dna[1:11])  # sequences between 1 to 11
+print(dna[5:])   # every letter after first 5 letters
+# some other functions are
+
+# length function
+print(len(dna))   # it tells the total length of the string
