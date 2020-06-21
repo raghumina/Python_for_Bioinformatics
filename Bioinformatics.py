c5b8814070597710d9944b3c7c4b8d9287389bb6
raghu <raghumina11@gmail.com>
2020-06-21 09:22:29 +0530

    Update Bioinformatics.py

--- Bioinformatics.py
+++ Bioinformatics.py
@@ -218,7 +218,7 @@ def has_stop_codon(dna,frame = 0):
 
 dna = "atgctgaaattgagagttatga"
 print(has_stop_codon(dna))
-'''
+
 #
 def function1(length):
     if length > 0:
@@ -242,3 +242,44 @@ n = -1
 x = 2
 y = 3
 print(compute(n,x,y))
+'''
+
+
+#
+def f(mystring):
+    print(message)
+    print(mystring)
+    message = "Inside function now!"
+    print(message)
+
+
+message = "Outside function!"
+f("Test function:")
+
+#
+def count1(dna, base):
+    i = 0
+    for c in dna:
+        if c == base:
+	    i += 1
+    return i
+
+def count2(dna, base):
+    i = 0
+    for j in range(len(dna)):
+        if dna[j] == base:
+	    i += 1
+    return i
+
+def count3(dna, base):
+    match = [c == base for c in dna]
+    return sum(match)
+
+def count4(dna, base):
+    return dna.count(base)
+
+def count5(dna, base):
+    return len([i for i in range(len(dna)) if dna[i] == base])
+
+def count6(dna,base):
+    return sum(c == base for c in dna)
