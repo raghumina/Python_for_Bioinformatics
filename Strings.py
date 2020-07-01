54fa4938fe008cfd9baead43ced6dea45a591632
raghu <raghumina11@gmail.com>
2020-07-01 17:08:10 +0530

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -13,3 +13,16 @@ print(''.join(seq))
 import random
 random.choice("ACGT")
 print(random.choice("ACGT"))
+
+
+# A fixed output in the random function
+print(random.seed(7))
+
+# for long random sequence
+#
+
+seq12 = ''
+for _ in range(100):
+    seq12 += random.choice("ATGC")
+    
+print(seq12.lower())
