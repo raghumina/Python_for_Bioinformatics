27fd5496d9a3810ac014c964fbd39499c9e5362d
raghu <raghumina11@gmail.com>
2020-07-01 16:24:37 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,15 @@
+# Sequence Strings
+# bio Sequences
+# import random
+#
+seq1 = 'ATGC'
+seq2 = 'GCAT'
+print(seq1 + seq2)
+seq3 = seq1 + seq2
+
+seq = ['A','T','G','C','T','G',]
+print(''.join(seq))
+
+import random
+random.choice("ACGT")
+print(random.choice("ACGT"))
