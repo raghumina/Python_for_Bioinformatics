534af5f9a3bd55d6bc474c6b7f0d3c629e7be8b2
raghu <raghumina11@gmail.com>
2020-07-12 22:18:54 +0530

    Update Strings.py

--- Strings.py
+++ Strings.py
@@ -79,3 +79,16 @@ str1 = "atgcatgc"
 str2 = "atgcatcgatcg"
 
 print(longpref(str1, str2))
+
+
+def match(s1, s2):
+    if not len(s1) == len(s2):
+        return False
+    for i in range(len(s1)):
+        if not s1[i] == s2[i]:
+            return False
+    return True
+
+s1 = "atgc"
+s2 = "attgc"
+print(match(s1, s2))
