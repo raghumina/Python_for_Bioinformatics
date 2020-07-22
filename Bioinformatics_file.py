5baf0d857f9051e3cada15a039495cc13697f110
raghu <raghumina11@gmail.com>
2020-07-22 14:50:51 +0530

    Update Bioinformatics_file.py

--- Bioinformatics_file.py
+++ Bioinformatics_file.py
@@ -1,17 +1,4 @@
-# a fasta file
-# FASTA format file
-'''
-
-
-# >AC073210.8 Homo sapiens BAC clone RP11-460N20 from 7, complete sequence
-
-X_File = open('Sequence.txt')
-for word in X_File:
-    print(X_File.read())
-
-x_file = open('Sequence.txt','w')
-x_file.write("the sequecne are\n")
-print(x_file)
-'''
-
-!wget--no--check
+# Reading a file
+# String manipulation
+f = open("sequence (2).fasta","r")
+print(f.read())
