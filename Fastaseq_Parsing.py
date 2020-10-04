1d1826411617f752d0df6e4c70ff0a8bf080c483
raghu <raghumina11@gmail.com>
2020-10-04 22:41:43 +0530

    Update Fastaseq_Parsing.py

--- Fastaseq_Parsing.py
+++ Fastaseq_Parsing.py
@@ -1,18 +1,19 @@
 
 #
-#from Bio import SeqIO
-#for seq_record in SeqIO.parse("seq.fasta","fasta"):
-#    print(seq_record.id)
-#    print(repr(seq_record.seq))
-#    print(len(seq_record))
+from Bio import SeqIO
+for seq_record in SeqIO.parse("seq.fasta","fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
 
 
 # now converting values into ascii
-data = [line.strip() for line in open("seq1.fasta", 'r')]
-print(count(data))
+#data = [line.strip() for line in open("seq1.fasta", 'r')]
+#a = data.count("A")
+#print(a)
 
-seq1 = []
-for  seq in data:
-    seq1.extend(ord(num) for num in seq)
-print(" %s"%(seq1))
+#seq1 = []
+#for  seq in data:
+#    seq1.extend(ord(num) for num in seq)
+#print(" %s"%(seq1))
 
