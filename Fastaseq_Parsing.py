1b16a6487a5abb0d8e261c35153dbca6a1346c4a
raghu <raghumina11@gmail.com>
2020-10-05 22:01:16 +0530

    Update Fastaseq_Parsing.py

--- Fastaseq_Parsing.py
+++ Fastaseq_Parsing.py
@@ -7,7 +7,6 @@ import pandas as pd
 #    print(repr(seq_record.seq))
 #    print(len(seq_record))
 
-''''
 # now converting values into ascii
 data = [line.strip() for line in open("seq1.fasta", 'r')]
 
@@ -22,12 +21,13 @@ for  seq in data:
     seq1.extend(ord(num) for num in seq)
     count = count + 1
 print(count, seq1)
-
+'''
 #f = open("seq1.fasta","a",encoding="utf-8")
 
 ##result = list(f.encoding(f))
 #print(result)
-'''
+
 
 sequence = ["ATGC"]
-result 
+result
+'''
