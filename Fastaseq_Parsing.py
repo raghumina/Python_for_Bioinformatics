f62e01f54f85899162791bd8971c5888a8454df9
raghu <raghumina11@gmail.com>
2020-10-05 09:56:34 +0530

    Update Fastaseq_Parsing.py

--- Fastaseq_Parsing.py
+++ Fastaseq_Parsing.py
@@ -1,5 +1,6 @@
+from pandas import DataFrame
+import pandas as pd
 
-#
 #from Bio import SeqIO
 #for seq_record in SeqIO.parse("seq.fasta","fasta"):
 #    print(seq_record.id)
@@ -10,14 +11,19 @@
 # now converting values into ascii
 data = [line.strip() for line in open("seq1.fasta", 'r')]
 
-print(data)
+#print(data)
+dataFrame = list([data])
+print(dataFrame)
 
-#seq1 = []
-#for  seq in data:
-#    seq1.extend(ord(num) for num in seq)
-#print(" %s"%(seq1))
+
+seq1 = []
+for  seq in data:
+    seq1.extend(ord(num) for num in seq)
+print(seq1)
 
 #f = open("seq1.fasta","a",encoding="utf-8")
 
 ##result = list(f.encoding(f))
 #print(result)
+
+
